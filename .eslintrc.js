a9fb79ee1ab7a64bd9406c00a64bc04437ff6cea
Andrei Rechkov <andrei.rechkov@gmail.com>
2025-04-03 19:31:28 +0200

    main: update global structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,14 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json',
-        sourceType: 'module'
-    },
-    plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended'
-    ],
     root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
     env: {
         node: true,
-        jest: true
+        es6: true,
     },
-    ignorePatterns: ['.eslintrc.js'],
-    rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
-    }
+    ignorePatterns: ['dist/', 'node_modules/'],
 };
